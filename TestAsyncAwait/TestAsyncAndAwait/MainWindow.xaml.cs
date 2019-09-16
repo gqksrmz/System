4ed8e71842c5efb58d23511fe7f3c5324fb89dc3
1825346742@qq.com <1825346742@qq.com>
2019-09-16 15:10:34 +0800

    wpf 异步async和await

---
+++ TestAsyncAwait/TestAsyncAndAwait/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAsyncAndAwait
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var task = await Save();
+            MessageBox.Show(task);
+        }
+        public async Task<string> Save()
+        {
+            button1.IsEnabled = false;
+            var wc = new WebClient();
+            var result = await wc.DownloadStringTaskAsync("https://www.baidu.com");
+            await Task.Run(() =>
+            {
+                MessageBox.Show(result);
+            });
+            button1.IsEnabled = true;
+
+            return "Success！";
+        }
+    }
+}
---
+++ TestAsyncAwait/TestAsyncAwait/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            var task = Save();
+            await task;
+        }
+        public async Task Save()
+        {
+            var wc = new WebClient();
+            var result = await wc.DownloadStringTaskAsync("https://www.baidu.com");
+            await Task.Run(() =>
+            {
+                MessageBox.Show(result);
+
+            });
+        }
+    }
+}
